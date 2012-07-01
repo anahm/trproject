2c1edff75acfe8244527fd88ed52bc5bc75c75e8
jjchen <junjunc@princeton.edu>
2012-07-01 07:42:00 -0700

    using Bing images

--- static/app.js
+++ static/app.js
@@ -61,7 +61,50 @@ function resetButtonClick() {
   gapi.hangout.data.submitDelta({'count': '0'});
 }
 
-function search() {
+    function search() 
+    {
+ 	//var term = document.forms["search_bar"]["searchTerm"].value;
+    
+        var search_input = $(".search_input").val();
+        	var searchLangSelect = document.getElementById('searchLang');
+	var searchLang = searchLangSelect.options[searchLangSelect.selectedIndex].value;
+
+        var keyword= encodeURIComponent(search_input);
+        // Bing Search API 
+        var bing_url='http://api.search.live.net/json.aspx?JsonType=callback&JsonCallback=?&Appid=5B0D22D739247C06BE7F990ECBEC1A144F9B7C39&query='+keyword+'&sources=image'; 
+
+    $.ajax
+    ({
+        type: "GET",
+        url: bing_url,
+        dataType:"jsonp",
+        success: function(response)
+        {
+            $("#result").html('');
+            if(response.SearchResponse.Image.Results.length)
+            {
+                $.each(response.SearchResponse.Image.Results, function(i,data)
+                {
+                    var title=data.Thumbnail.Url;
+                    //var dis=data.Description;
+                    var url=data.MediaUrl;
+
+
+                    var final="<div class='webresult'><div class='title'><img src='"+title+"'>";
+
+                    $("#result").append(final); // Result
+
+                });
+            }
+            else
+            {
+                $("#result").html("<div id='no'>No Results</div>");
+            }
+        }
+    });
+};
+
+/* function search() {
 	var term = document.forms["search_bar"]["searchTerm"].value;
 	var langSelect = document.getElementById('langselect');
     var selectedLang = langSelect.options[langSelect.selectedIndex].value;
@@ -118,7 +161,7 @@ function OnLoad(text) {
                 // Include the required Google branding
         google.search.Search.getBranding('branding');
 }
-
+*/
 
 var forbiddenCharacters = /[^a-zA-Z!0-9_\- ]/;
 function setText(element, text) {
@@ -141,7 +184,7 @@ function onMessageReceived(event) {
      console.log(e);
   }
 }
-
+/*
 // Load the Google Transliterate API
       google.load("elements", "1", {
             packages: "transliteration"
@@ -171,7 +214,7 @@ function onMessageReceived(event) {
 
 google.setOnLoadCallback(onLoadTransliterate);
       
-
+*/
 function getMessageClick() {
   console.log('Requesting message from main.py');
   var http = new XMLHttpRequest();
@@ -205,6 +248,7 @@ function getTranslatedText(text, transfrom, transto) {
   var post_data = "from=" + transfrom + "&to=" + transto + "&text=" + text;
   http.send(post_data);
 }
+
 
 function updateStateUi(state) {
   var countElement = document.getElementById('count');
